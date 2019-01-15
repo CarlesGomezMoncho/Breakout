da7d0bb8699f04649c7d13de574c2afb6471c1b1
Carles <carlesgomo@gmail.com>
2019-01-15 13:15:48 +0100

    algunos objetos iniciales
    Añadidas una raqueta y una pelota con material fisico para la pelota y sprite para la raqueta, ambos tienen su collider. El sprite de la pelota es un redondo de Unity.
    También se ha añadido un playerController, donde de momento se controla la el movimiento de la raqueta del jugador.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        if (horizontal != 0)
+        {
+            transform.position = new Vector2(transform.position.x + horizontal * movementSpeed * Time.deltaTime, transform.position.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
