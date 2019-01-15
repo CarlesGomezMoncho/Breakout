3daa6bac1c10f762b9018d2b7b9df1573f8470fc
Carles <carlesgomo@gmail.com>
2019-01-15 18:56:49 +0100

    Iniciados controladores de pelota y juego
    También se ha añadido un tilemap y una paleta de prueba

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Transform centroRaqueta;
+
+    public float impulseForce = 2;
+
+    private bool iniciada = false;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!iniciada)
+            transform.position = centroRaqueta.position;
+
+    }
+
+    public void Iniciar()
+    {
+        iniciada = true;
+
+        rb.AddForce(new Vector2(0, 1 * impulseForce), ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;
+
+    public GameObject raqueta;
+    public Transform centroRaqueta;
+    public GameObject pelota;
+
+    private BallController ballController;
+
+    private bool gameStarted = false;
+
+    private void Awake()
+    {
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        StartGame();
+
+        ballController = pelota.GetComponent<BallController>();
+        ballController.centroRaqueta = centroRaqueta;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !gameStarted)
+        {
+            ballController.Iniciar();
+            gameStarted = true;
+        }
+
+    }
+
+    public void StartGame()
+    {
+        pelota.transform.position = new Vector2(centroRaqueta.position.x, centroRaqueta.transform.position.y);
+    }
+}
