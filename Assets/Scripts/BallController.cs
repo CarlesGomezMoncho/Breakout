80ec707304c58d5eafc95a0ded701238456233e2
Carles <carlesgomo@gmail.com>
2019-01-17 16:34:14 +0100

    Reajustes comportamiento pelota
    Eliminado el script GridController, eliminar el tile con el que choca la pelota se hace desde la pelota.
    Añadido un reajuste de dirección cuando la pelota golpea la requeta en los bordes.
    Añadido también un reajuste si la pelota se mantine recta en horizontal o en vertical (velocidad y = 0 o X = 0)
    Añadido paquete esencial de TMPro, para texto.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class BallController : MonoBehaviour
 {
     public Transform centroRaqueta;
     public Tilemap tileMap;
+    public TextMeshProUGUI text;
 
     public float fuerzaInicial = 2;
 
@@ -23,6 +25,21 @@ public class BallController : MonoBehaviour
         if (!iniciada)
             transform.position = centroRaqueta.position;
 
+        //si se queda fijo en un eje, le modificamos un poco su velocidad
+        if(rb.velocity.x == 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(1, 0);
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            rb.velocity = rb.velocity + new Vector2(0, 1);
+        }
+
+        if (text)
+        {
+            text.text = rb.velocity.ToString();
+        }
     }
 
     private void FixedUpdate()
@@ -59,6 +76,7 @@ public class BallController : MonoBehaviour
 
         int randY = 1;
         float randX = Random.Range(-0.5f, 0.5f); //valores entre -45º y 45º
+        //float randX = 0;  //para debug, sale completamente vertical
 
         direction = new Vector2(randX, randY);
 
@@ -67,27 +85,48 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector3 hitPosition = Vector3.zero;
-        Vector3 hitPositionI = Vector3.zero;    //por si golpea muy cerca de otro tile a la izquierda
-        Vector3 hitPositionD = Vector3.zero;    //igual pero derecha
-
-        foreach (ContactPoint2D hit in collision.contacts)
+        if (collision.gameObject.name == "Tilemap")
         {
-            hitPosition.x = hit.point.x;
-            hitPosition.y = hit.point.y;
+            Vector3 hitPosition = Vector3.zero;
+            Vector3 hitPositionI = Vector3.zero;    //por si golpea muy cerca de otro tile a la izquierda
+            Vector3 hitPositionD = Vector3.zero;    //igual pero derecha
 
-            hitPositionI.x = hit.point.x -0.1f;
-            hitPositionI.y = hit.point.y;
+            //Solo si está cerca de otro tile se crearán distintos hitPosition, sinó está cerca se pondrá a null el mismo tile 3 veces
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x;
+                hitPosition.y = hit.point.y;
 
-            hitPositionD.x = hit.point.x + 0.1f;
-            hitPositionD.y = hit.point.y;
+                hitPositionI.x = hit.point.x - 0.1f;
+                hitPositionI.y = hit.point.y;
 
-            tileMap.SetTile(tileMap.WorldToCell(hitPosition), null);
-            tileMap.SetTile(tileMap.WorldToCell(hitPositionI), null);
-            tileMap.SetTile(tileMap.WorldToCell(hitPositionD), null);
+                hitPositionD.x = hit.point.x + 0.1f;
+                hitPositionD.y = hit.point.y;
 
-            //Debug.Log(hitPosition.ToString() + " - " + hitPositionI.ToString() + " -> " + tileMap.WorldToCell(hitPosition) + " - " + tileMap.WorldToCell(hitPositionI));
+                tileMap.SetTile(tileMap.WorldToCell(hitPosition), null);
+                tileMap.SetTile(tileMap.WorldToCell(hitPositionI), null);
+                tileMap.SetTile(tileMap.WorldToCell(hitPositionD), null);
+            }
         }
+        else if (collision.gameObject.name == "Raqueta")
+        {
+            //Modificamos el grado de la dirección de la pelota dependiendo donde colisiona la pelota con la raqueta
+            //cuando más se acerque al borde derecho, más se acercará 1 (la desviación)
+            //si esta en el centro será 0
+            //cuando más se acerque al borde izquierdo, más se acercará a -1
 
+            //restamos a la posición de la raqueta, la posición de la pelota, para conocer la posición relativa entre ellas dos en el eje x
+            float x = (transform.position.x - collision.transform.position.x);
+
+            //si es menor que 0.5 o mayor que -0.5 vamos a indicar que golpea en el centro de la raqueta, por tanto no habrá una modficación en la dirección
+            if (x < 0.5f && x > -0.5f)
+                x = 0;
+
+            //modificamos la velocidad actual normalizando el valor obtenido antes, de tal forma que si golpea en el centro de la raqueta la pelota no modifica
+            //la dirección. En cambio si golpea cerca de los bordes, se modifica la dirección actual en +1 (si golpea en la parte derecha) o en -1
+            //(si golpea en la parte izquierda) como el valor sera un valor decimal se normaliza, para que si es mayor que 0 sea 1 y si es menor que 0
+            //sea -1 y asi se modificará en cada golpeo la dirección de la pelota en +1, 0 o -1 dependiendo de la zona en la que golpee la pelota en la raqueta.
+            rb.velocity = rb.velocity + new Vector2(x, 0).normalized;
+        }
     }
 }
